92a9fa2757755946ccebdcf9cbd9c1a59b57a683
rogeriocassares <roger10cassares@gmail.com>
2023-01-14 23:32:35 -0300

    fix: clone-all.sh script

--- clone-all.sh
+++ clone-all.sh
@@ -2,13 +2,22 @@
 
 cd ..
 
-git clone https://github.com/OpenDataTelemetry/device-data-esp32-idf.git.github
-git clone https://github.com/OpenDataTelemetry/gateway-grpc.github
-git clone https://github.com/OpenDataTelemetry/gateway-http.github
-git clone https://github.com/OpenDataTelemetry/gateway-mqtt.github
-git clone https://github.com/OpenDataTelemetry/gateway-udp.github
-git clone https://github.com/OpenDataTelemetry/gateway-tcp.github
-git clone https://github.com/OpenDataTelemetry/gateway-ws.github
-git clone https://github.com/OpenDataTelemetry/gateway-to-broker.github
-git clone https://github.com/OpenDataTelemetry/event-straming.github
-git clone https://github.com/OpenDataTelemetry/timeseries-database.github
+git clone https://github.com/OpenDataTelemetry/device-data-esp32-idf.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-grpc.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-http.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-mqtt.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-udp.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-tcp.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-ws.git
+
+git clone https://github.com/OpenDataTelemetry/gateway-to-broker.git
+
+git clone https://github.com/OpenDataTelemetry/event-straming.git
+
+git clone https://github.com/OpenDataTelemetry/timeseries-database.git
