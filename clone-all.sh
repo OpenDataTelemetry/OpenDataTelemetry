61274e8020acacdc787692999438a18e85e9ee24
rogeriocassares <roger10cassares@gmail.com>
2023-01-14 23:22:51 -0300

    feat: add clone-all.sh script

---
+++ clone-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+cd ..
+
+git clone https://github.com/OpenDataTelemetry/device-data-esp32-idf.git.github
+git clone https://github.com/OpenDataTelemetry/gateway-grpc.github
+git clone https://github.com/OpenDataTelemetry/gateway-http.github
+git clone https://github.com/OpenDataTelemetry/gateway-mqtt.github
+git clone https://github.com/OpenDataTelemetry/gateway-udp.github
+git clone https://github.com/OpenDataTelemetry/gateway-tcp.github
+git clone https://github.com/OpenDataTelemetry/gateway-ws.github
+git clone https://github.com/OpenDataTelemetry/gateway-to-broker.github
+git clone https://github.com/OpenDataTelemetry/event-straming.github
+git clone https://github.com/OpenDataTelemetry/timeseries-database.github
